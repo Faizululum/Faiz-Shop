8506369a7b131305bf8f1144c87298325b04d4e8
Faizululum <faizululum25@gmail.com>
2025-01-19 21:49:36 +0700

    Create Slider Component (#04)

---
+++ src/components/Slider.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const slides = [
+  {
+    id: 1,
+    title: "Summer Sale Collection",
+    description: "Sale Up To 50% Off On Summer Sale Collection",
+    img: "https://images.pexels.com/photos/3620411/pexels-photo-3620411.jpeg?auto=compress&cs=tinysrgb&w=600",
+    url: "/",
+    bg: "bg-gradient-to-r from-yellow-50 to-pink-50",
+  },
+  {
+    id: 2,
+    title: "Winter Sale Collection",
+    description: "Sale Up To 50% Off On Winter Sale Collection",
+    img: "https://images.pexels.com/photos/157675/fashion-men-s-individuality-black-and-white-157675.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    url: "/",
+    bg: "bg-gradient-to-r from-yellow-50 to-pink-50",
+  },
+  {
+    id: 3,
+    title: "Spring Sale Collection",
+    description: "Sale Up To 50% Off On Spring Sale Collection",
+    img: "https://images.pexels.com/photos/20663750/pexels-photo-20663750/free-photo-of-woman-in-pink-suit.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    url: "/",
+    bg: "bg-gradient-to-r from-yellow-50 to-pink-50",
+  },
+];
+
+const Slider = () => {
+  const [activeSlide, setActiveSlide] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveSlide((prev) => (prev === slides.length - 1 ? 0 : prev + 1));
+    }, 3000);
+
+    return () => clearInterval(interval);
+  });
+
+  return (
+    <div className="h-[calc(100vh-80px)] overflow-hidden">
+      <div
+        className="w-max h-full flex transition-all ease-in-out duration-1000"
+        style={{ transform: `translateX(-${100 * activeSlide}vw)` }}
+      >
+        {slides.map((slide) => (
+          <div
+            className={`${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row`}
+            key={slide.id}
+          >
+            {/* TEXT CONTAINER */}
+            <div className="h-1/2 xl:w-1/2 xl:h-full flex flex-col items-center justify-center gap-8 xl:gap-12 text-center">
+              <h2 className="text-xl lg:text-3xl 2xl:text-5xl">
+                {slide.description}
+              </h2>
+              <h1 className="text-5xl lg:text-6xl 2xl:text-8xl font-semibold">
+                {slide.title}
+              </h1>
+              <Link href={slide.url}>
+                <button className="rounded-md bg-black py-3 px-4 text-white">
+                  SHOP NOW
+                </button>
+              </Link>
+            </div>
+            {/* IMAGE CONTAINER */}
+            <div className="h-1/2 xl:w-1/2 xl:h-full relative">
+              <Image
+                src={slide.img}
+                alt=""
+                fill
+                sizes="100%"
+                className="object-cover"
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="absolute m-auto left-1/2 bottom-8 flex gap-4">
+        {slides.map((slide, index) => (
+          <div
+            className={`h-3 w-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${
+              activeSlide === index ? "scale-150" : ""
+            }`}
+            key={slide.id}
+            onClick={() => setActiveSlide(index)}
+          >
+            {activeSlide === index && (
+              <div className="w-[6px] h-[6px] bg-gray-600 rounded-full"></div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
