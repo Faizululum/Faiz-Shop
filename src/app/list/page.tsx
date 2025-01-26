3f6ec2d97ae024acb761a33e81ad54a3d271bff8
Faizululum <faizululum25@gmail.com>
2025-01-26 12:45:23 +0700

    Fetch Categories (with Wix) (#13)

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,29 +1,48 @@
-import Filter from "@/components/Filter"
-import ProductList from "@/components/ProductList"
-import Image from "next/image"
+import Filter from "@/components/Filter";
+import ProductList from "@/components/ProductList";
+import { wixClientServer } from "@/lib/wixClientServer";
+import Image from "next/image";
+import { Suspense } from "react";
 
-const ListPage = () => {
+const ListPage = async ({ searchParams }: { searchParams: any }) => {
+  const wixClient = await wixClientServer();
+
+  const cat = await wixClient.collections.getCollectionBySlug(
+    searchParams.cat || "all-products"
+  );
+
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
-        {/* CAMPAIGN */}
-        <div className="bg-pink-50 px-4 flex justify-between h-64">
-            <div className="hidden w-2/3 sm:flex flex-col items-center justify-center gap-8">
-                <h1 className="text-4xl font-semibold leading-[48px]  text-gray-700">Grab up to 50% off on 
-                  <br />Selected Products
-                </h1>
-                <button className="rounded-3xl bg-red text-white w-max py-3 px-5 text-sm">Buy Now</button>
-            </div>
-            <div className="relative w-1/3">
-                <Image src="/woman.png" alt="woman" fill className="object-contain" />
-            </div>
+      {/* CAMPAIGN */}
+      <div className="bg-pink-50 px-4 flex justify-between h-64">
+        <div className="hidden w-2/3 sm:flex flex-col items-center justify-center gap-8">
+          <h1 className="text-4xl font-semibold leading-[48px]  text-gray-700">
+            Grab up to 50% off on
+            <br />
+            Selected Products
+          </h1>
+          <button className="rounded-3xl bg-red text-white w-max py-3 px-5 text-sm">
+            Buy Now
+          </button>
         </div>
-        {/* FILTER */}
-        <Filter />
-        {/* PRODUCTS */}
-        <h1 className="mt-12 text-xl font-semibold">Dynamic For You!</h1>
-        <ProductList />
+        <div className="relative w-1/3">
+          <Image src="/woman.png" alt="woman" fill className="object-contain" />
+        </div>
+      </div>
+      {/* FILTER */}
+      <Filter />
+      {/* PRODUCTS */}
+      <h1 className="mt-12 text-xl font-semibold">Dynamic For You!</h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ProductList
+          categoryId={
+            cat.collection?._id || "00000000-000000-000000-000000000001"
+          }
+          searchParams={searchParams}
+        />
+      </Suspense>
     </div>
-  )
-}
+  );
+};
 
-export default ListPage
+export default ListPage;
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -9,9 +9,11 @@ const PRODUCT_PER_PAGE = 20;
 const ProductList = async ({
   categoryId,
   limit,
+  searchParams,
 }: {
   categoryId: string;
   limit?: number;
+  searchParams: any;
 }) => {
   const wixClient = await wixClientServer();
 
@@ -25,7 +27,7 @@ const ProductList = async ({
     <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
       {res.items.map((product: products.Product) => (
         <Link
-          href={"/"+product.slug}
+          href={"/" + product.slug}
           className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
           key={product._id}
         >
@@ -39,29 +41,35 @@ const ProductList = async ({
             />
             {product.media?.items && (
               <Image
-              src={product.media?.items[1]?.image?.url || "/product.png"}
-              alt="product"
-              fill
-              sizes="25vw"
-              className="absolute object-cover rounded-md"
-            />)}
+                src={product.media?.items[1]?.image?.url || "/product.png"}
+                alt="product"
+                fill
+                sizes="25vw"
+                className="absolute object-cover rounded-md"
+              />
+            )}
           </div>
           <div className="flex justify-between">
             <span className="font-medium">{product.name}</span>
             <span className="font-semibold">
-              IDR {new Intl.NumberFormat('id-ID').format((product.price?.price || 0) * 1000)},00
-            </span> 
+              IDR{" "}
+              {new Intl.NumberFormat("id-ID").format(
+                (product.price?.price || 0) * 1000
+              )}
+              ,00
+            </span>
           </div>
-          {product.additionalInfoSections && 
-          (
-          <div className="text-sm text-gray-500" dangerouslySetInnerHTML={{ 
-            __html: DOMPurify.sanitize(
-              product.additionalInfoSections.find(
-                (section: any)=>section.title==="shortDesc"
-              )?.description || ""
-              ), 
-            }}
-          ></div>
+          {product.additionalInfoSections && (
+            <div
+              className="text-sm text-gray-500"
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(
+                  product.additionalInfoSections.find(
+                    (section: any) => section.title === "shortDesc"
+                  )?.description || ""
+                ),
+              }}
+            ></div>
           )}
           <button className="rounded-2xl ring-1 ring-red text-red py-2 px-4 w-max text-xs hover:bg-red hover:text-white">
             Add to Cart
