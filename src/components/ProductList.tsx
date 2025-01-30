5e18c5618d21cf2bf7d66d87459ca8d9f03a06bd
Faizululum <faizululum25@gmail.com>
2025-01-30 10:59:50 +0700

    Fetch & Update Filter Products (#17)

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -13,15 +13,34 @@ const ProductList = async ({
 }: {
   categoryId: string;
   limit?: number;
-  searchParams: any;
+  searchParams?: any;
 }) => {
   const wixClient = await wixClientServer();
 
-  const res = await wixClient.products
+  const productQuery = wixClient.products
     .queryProducts()
+    .startsWith("name", searchParams?.name || "")
     .eq("collectionIds", categoryId)
-    .limit(limit || PRODUCT_PER_PAGE)
-    .find();
+    .hasSome(
+      "productType",
+      searchParams?.type ? [searchParams.type] : ["physical", "digital"]
+    )
+    .gt("priceData.price", searchParams?.min || 0)
+    .lt("priceData.price", searchParams?.max || 10000)
+    .limit(limit || PRODUCT_PER_PAGE);
+
+  if (searchParams?.sort) {
+    const [sortType, sortBy] = searchParams.sort.split(" ");
+
+    if (sortType === "asc") {
+      productQuery.ascending(sortBy);
+    }
+    if (sortType === "desc") {
+      productQuery.descending(sortBy);
+    }
+  }
+
+  const res = await productQuery.find();
 
   return (
     <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
